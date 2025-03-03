3bef57a6b92bb13ab49eabbae0075afc6fea3e5b
AR123456 <coloradoz99@gmail.com>
2025-03-03 09:03:17 -0500

    for loop way

--- Section 18 Advanced JS and DOM manipulation/Drum Kit/app.js
+++ Section 18 Advanced JS and DOM manipulation/Drum Kit/app.js
@@ -3,6 +3,27 @@ let drumButtons = document.querySelectorAll(".drum");
 
 for (let i = 0; i < drumButtons.length; i++) {
   drumButtons[i].addEventListener("click", () => {
-    new Audio("./sounds/crash.mp3").play();
+    drumButtons[i].style.color = "white";
+    if (drumButtons[i].innerHTML == "w") {
+      new Audio("./sounds/crash.mp3").play();
+    }
+    if (drumButtons[i].innerHTML == "a") {
+      new Audio("./sounds/kick-bass.mp3").play();
+    }
+    if (drumButtons[i].innerHTML == "s") {
+      new Audio("./sounds/snare.mp3").play();
+    }
+    if (drumButtons[i].innerHTML == "d") {
+      new Audio("./sounds/tom-1.mp3").play();
+    }
+    if (drumButtons[i].innerHTML == "j") {
+      new Audio("./sounds/tom-2.mp3").play();
+    }
+    if (drumButtons[i].innerHTML == "k") {
+      new Audio("./sounds/tom-3.mp3").play();
+    }
+    if (drumButtons[i].innerHTML == "l") {
+      new Audio("./sounds/tom-4.mp3").play();
+    }
   });
 }
