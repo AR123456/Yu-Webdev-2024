60ec9d66771ff4f4643e7cda431db48d2fae1134
AR123456 <coloradoz99@gmail.com>
2025-03-03 09:03:55 -0500

    switch is better

--- Section 18 Advanced JS and DOM manipulation/Drum Kit/app.js
+++ Section 18 Advanced JS and DOM manipulation/Drum Kit/app.js
@@ -4,26 +4,38 @@ let drumButtons = document.querySelectorAll(".drum");
 for (let i = 0; i < drumButtons.length; i++) {
   drumButtons[i].addEventListener("click", () => {
     drumButtons[i].style.color = "white";
-    if (drumButtons[i].innerHTML == "w") {
-      new Audio("./sounds/crash.mp3").play();
-    }
-    if (drumButtons[i].innerHTML == "a") {
-      new Audio("./sounds/kick-bass.mp3").play();
-    }
-    if (drumButtons[i].innerHTML == "s") {
-      new Audio("./sounds/snare.mp3").play();
-    }
-    if (drumButtons[i].innerHTML == "d") {
-      new Audio("./sounds/tom-1.mp3").play();
-    }
-    if (drumButtons[i].innerHTML == "j") {
-      new Audio("./sounds/tom-2.mp3").play();
-    }
-    if (drumButtons[i].innerHTML == "k") {
-      new Audio("./sounds/tom-3.mp3").play();
-    }
-    if (drumButtons[i].innerHTML == "l") {
-      new Audio("./sounds/tom-4.mp3").play();
+
+    switch (drumButtons[i].innerHTML) {
+      case "w":
+        new Audio("./sounds/crash.mp3").play();
+        break;
+
+      case "a":
+        new Audio("./sounds/kick-bass.mp3").play();
+        break;
+
+      case "s":
+        new Audio("./sounds/crash.mp3").play();
+        break;
+
+      case "d":
+        new Audio("./sounds/tom-1.mp3").play();
+        break;
+
+      case "j":
+        new Audio("./sounds/tom-2.mp3").play();
+        break;
+
+      case "k":
+        new Audio("./sounds/tom-3.mp3").play();
+        break;
+
+      case "l":
+        new Audio("./sounds/tom-4.mp3").play();
+        break;
+
+      default:
+        break;
     }
   });
 }
