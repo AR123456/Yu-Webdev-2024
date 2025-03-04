f61cb6e17cfa8cd48d8c33bc0de46ec9f57e6252
AR123456 <coloradoz99@gmail.com>
2025-03-04 08:02:39 -0500

    drums keyup or click

--- Section 18 Advanced JS and DOM manipulation/Drum Kit/app.js
+++ Section 18 Advanced JS and DOM manipulation/Drum Kit/app.js
@@ -3,13 +3,10 @@ let drumButtons = document.querySelectorAll(".drum");
 
 for (let i = 0; i < drumButtons.length; i++) {
   drumButtons[i].addEventListener("click", () => {
-    drumButtons[i].style.color = "white";
-
     switch (drumButtons[i].innerHTML) {
       case "w":
         new Audio("./sounds/crash.mp3").play();
         break;
-
       case "a":
         new Audio("./sounds/kick-bass.mp3").play();
         break;
@@ -35,7 +32,42 @@ for (let i = 0; i < drumButtons.length; i++) {
         break;
 
       default:
+        console.log(drumButtons[i].innerHTML);
         break;
     }
   });
 }
+window.addEventListener("keyup", (e) => {
+  switch (e.key) {
+    case "w":
+      new Audio("./sounds/crash.mp3").play();
+      break;
+    case "a":
+      new Audio("./sounds/kick-bass.mp3").play();
+      break;
+
+    case "s":
+      new Audio("./sounds/crash.mp3").play();
+      break;
+
+    case "d":
+      new Audio("./sounds/tom-1.mp3").play();
+      break;
+
+    case "j":
+      new Audio("./sounds/tom-2.mp3").play();
+      break;
+
+    case "k":
+      new Audio("./sounds/tom-3.mp3").play();
+      break;
+
+    case "l":
+      new Audio("./sounds/tom-4.mp3").play();
+      break;
+
+    default:
+      console.log(key);
+      break;
+  }
+});
