a9eb01561dd63f7014dd9b3dc1990676b92ec6c4
AR123456 <coloradoz99@gmail.com>
2025-03-04 08:03:39 -0500

    back to 2 functions

--- Section 18 Advanced JS and DOM manipulation/Drum Kit/app.js
+++ Section 18 Advanced JS and DOM manipulation/Drum Kit/app.js
@@ -37,42 +37,7 @@ for (let i = 0; i < drumButtons.length; i++) {
     }
   });
 }
-// document.addEventListener("keyup", (e) => {
-//   switch (e.key) {
-//     case "w":
-//       new Audio("./sounds/crash.mp3").play();
-//       break;
-//     case "a":
-//       new Audio("./sounds/kick-bass.mp3").play();
-//       break;
-
-//     case "s":
-//       new Audio("./sounds/crash.mp3").play();
-//       break;
-
-//     case "d":
-//       new Audio("./sounds/tom-1.mp3").play();
-//       break;
-
-//     case "j":
-//       new Audio("./sounds/tom-2.mp3").play();
-//       break;
-
-//     case "k":
-//       new Audio("./sounds/tom-3.mp3").play();
-//       break;
-
-//     case "l":
-//       new Audio("./sounds/tom-4.mp3").play();
-//       break;
-
-//     default:
-//       console.log(key);
-//       break;
-//   }
-// });
-
-const runSwitch = (e) => {
+document.addEventListener("keyup", (e) => {
   switch (e.key) {
     case "w":
       new Audio("./sounds/crash.mp3").play();
@@ -105,5 +70,4 @@ const runSwitch = (e) => {
       console.log(key);
       break;
   }
-};
-document.addEventListener("keyup", runSwitch);
+});
