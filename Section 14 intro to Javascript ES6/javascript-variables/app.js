c68a2cd53d575f6e0c9db6c9ddd8342449f72647
AR123456 <coloradoz99@gmail.com>
2025-03-07 06:39:00 -0500

    starter

--- Section 14 intro to Javascript ES6/javascript-variables/app.js
+++ Section 14 intro to Javascript ES6/javascript-variables/app.js
@@ -1,1 +1,13 @@
 // switch vars around
+function test() {
+  var a = "3";
+  var b = "8";
+
+  /***********Do not change the code above 👆*******/
+  //Write your code on lines 7 - 9:
+
+  /***********Do not change the code below 👇*******/
+
+  console.log("a is " + a);
+  console.log("b is " + b);
+}
