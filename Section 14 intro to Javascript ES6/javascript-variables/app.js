be64e3fa649499a0caf5ba26f0d235ada37e551f
AR123456 <coloradoz99@gmail.com>
2025-03-07 06:38:23 -0500

    alert response in var

--- Section 14 intro to Javascript ES6/javascript-variables/app.js
+++ Section 14 intro to Javascript ES6/javascript-variables/app.js
@@ -1,12 +1,2 @@
-// Dog to human age Converter
-// humanAge = (dogAge -2) X 4 +21
-
-// let inputAge = prompt("What is the dog age ?");
-// //
-// let humanAge = (inputAge - 2) * 4 + 21;
-
-// alert(`Dog age of ${inputAge} is ${humanAge} human years `);
-
-let x = 3;
-let y = x++;
-console.log(y);
+const yourName = prompt("What is your name");
+alert(yourName);
