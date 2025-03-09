d76aebb90d44c3085d262ff49513ecadf584f14f
AR123456 <coloradoz99@gmail.com>
2025-03-09 05:37:37 -0400

    string length and number of characters

--- Section 14 intro to Javascript ES6/javascript-variables/app.js
+++ Section 14 intro to Javascript ES6/javascript-variables/app.js
@@ -1,3 +1,9 @@
 // length property
 let string = "some number";
 console.log(string.length);
+// prompt for string
+let max = 145;
+let textUsed = prompt("Give me some text");
+alert(
+  `That is ${textUsed.length} , you have ${145 - textUsed.length} remaining`
+);
