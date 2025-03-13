402ce069d69738e36a05e0a43ae11c34bcd3e6dd
AR123456 <coloradoz99@gmail.com>
2025-03-13 04:31:33 -0400

    sending template string

--- Section 24 Express and node/3.2 HTTP Requests/index.js
+++ Section 24 Express and node/3.2 HTTP Requests/index.js
@@ -5,7 +5,8 @@ const port = 3000;
 // get express method
 // "/" is root or home page
 app.get("/", (req, res) => {
-  res.send("Hello from the server");
+  // res.send("Hello from the server");
+  res.send(` <h1>How about a text template ? </h1>`);
 });
 app.listen(port, () => {
   console.log(`App is listening on port http://localhost:${port}`);
