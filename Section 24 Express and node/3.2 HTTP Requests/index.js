2163d8d5e9ffb0a9c18993842efb87db90c977c3
AR123456 <coloradoz99@gmail.com>
2025-03-13 04:32:38 -0400

    http requests

--- Section 24 Express and node/3.2 HTTP Requests/index.js
+++ Section 24 Express and node/3.2 HTTP Requests/index.js
@@ -5,10 +5,16 @@ const port = 3000;
 // get express method
 // "/" is root or home page
 app.get("/", (req, res) => {
-  // res.send("Hello from the server");
-  // res.send(` <h1>How about a text template ? </h1>`);
-  console.log(req.rawHeaders);
+  res.send(` <h1>Home is "/" </h1>`);
 });
+// "/" is root or home page
+app.get("/about", (req, res) => {
+  res.send(` <h1>This is the about end point </h1>`);
+});
+app.get("/contact", (req, res) => {
+  res.send(` <h1>Getting the contact page </h1>`);
+});
+
 app.listen(port, () => {
   console.log(`App is listening on port http://localhost:${port}`);
 });
