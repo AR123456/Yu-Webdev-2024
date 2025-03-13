ea3946362a495d4e291c538c8d95af9ef5ff2f44
AR123456 <coloradoz99@gmail.com>
2025-03-13 04:33:47 -0400

    random number generator

---
+++ Section 15 Intermediate Javascript/Section 15 - Random Number Generator/app.js
@@ -0,0 +1,11 @@
+const randomNum = Math.random();
+console.log(Math.floor(randomNum * 6) + 1);
+
+// or
+const randNum = Math.floor(Math.random() * 6 + 1);
+console.log(randNum);
+const n1=input("Name One")
+const n2=input("Name two ")
+
+
+alert(`Your love percent is ${randPercent}`)
