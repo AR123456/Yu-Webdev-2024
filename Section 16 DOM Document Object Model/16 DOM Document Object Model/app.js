f7c031625d3b7f90747508b993641227f4648ca8
AR123456 <coloradoz99@gmail.com>
2025-03-15 08:17:28 -0400

    DOM starter files

---
+++ Section 16 DOM Document Object Model/16 DOM Document Object Model/app.js
@@ -0,0 +1,4 @@
+// let body = document.firstElementChild.lastElementChild;
+// console.log(body);
+// let h1 = document.firstElementChild.lastElementChild.firstElementChild;
+document.querySelector("input").click();
