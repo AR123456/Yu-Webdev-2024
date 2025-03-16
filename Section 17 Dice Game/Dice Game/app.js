04153ffce025d06b1313b3200d6a47b733dac845
AR123456 <coloradoz99@gmail.com>
2025-03-16 06:50:01 -0400

    adding js

--- Section 17 Dice Game/Dice Game/app.js
+++ Section 17 Dice Game/Dice Game/app.js
@@ -10,3 +10,13 @@
 // create display of the six dot pattern
 // draw the dots based on the value
 // display player ___ wins in div  over both dice
+window.addEventListener("load", () => {
+  const player1 = document.querySelector(".player-1");
+  const player2 = document.querySelector(".player-2");
+  const roll = () => {
+    let dice1 = Math.floor(Math.random() * 6 + 1);
+    let dice2 = Math.floor(Math.random() * 6 + 1);
+    console.log(dice1, dice2);
+  };
+  roll();
+});
