8ce83b656d72f80e62690163b1926e0b657b3e47
AR123456 <coloradoz99@gmail.com>
2025-03-16 06:49:38 -0400

    psudo code

---
+++ Section 17 Dice Game/Dice Game/app.js
@@ -0,0 +1,12 @@
+// function to generate a random number
+
+// generate 2 random numbers identify player one and player 2
+
+// compare the 2 numbers
+// larger wins if equal draw
+
+// display values of dice roll for player one and two in dom
+
+// create display of the six dot pattern
+// draw the dots based on the value
+// display player ___ wins in div  over both dice
