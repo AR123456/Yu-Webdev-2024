c1ed1dcb075a0415293e894c07927f7a0e31246b
AR123456 <coloradoz99@gmail.com>
2025-03-18 05:45:39 -0400

    adding dice from js

--- Section 17 Dice Game/Dice Game/app.js
+++ Section 17 Dice Game/Dice Game/app.js
@@ -11,8 +11,56 @@
 // draw the dots based on the value
 // display player ___ wins in div  over both dice
 window.addEventListener("load", () => {
+  // consts
+  const diceFaceOne = `<div class="dice one-face">
+          <span class="dot"></span>
+        </div>`;
+  const diceFaceTwo = `<div class="dice two-face">
+          <span class="dot"> </span>
+          <span class="dot"> </span>
+        </div>`;
+  const diceFaceThree = `<div class="dice three-face">
+          <span class="dot"></span>
+          <span class="dot"></span>
+          <span class="dot"></span>
+        </div>`;
+  const diceFaceFour = `<div class="dice four-face">
+          <div class="column">
+            <span class="dot"></span>
+            <span class="dot"></span>
+          </div>
+          <div class="column">
+            <span class="dot"></span>
+            <span class="dot"></span>
+          </div>
+        </div>`;
+  const diceFaceFive = `<div class="dice five-face">
+          <div class="column">
+            <span class="dot"></span>
+            <span class="dot"></span>
+          </div>
+          <div class="column">
+            <span class="dot"></span>
+          </div>
+          <div class="column">
+            <span class="dot"></span>
+            <span class="dot"></span>
+          </div>
+        </div>`;
+  const diceFaceSix = `<div class="dice six-face">
+          <div class="column">
+            <span class="dot"></span>
+            <span class="dot"></span><span class="dot"></span>
+          </div>
+          <div class="column">
+            <span class="dot"></span>
+            <span class="dot"></span><span class="dot"></span>
+          </div>
+        </div>`;
   const player1 = document.querySelector(".player-1");
   const player2 = document.querySelector(".player-2");
+  player1.innerHTML = diceFaceSix;
+  player2.innerHTML = diceFaceSix;
   const roll = () => {
     let dice1 = Math.floor(Math.random() * 6 + 1);
     let dice2 = Math.floor(Math.random() * 6 + 1);
