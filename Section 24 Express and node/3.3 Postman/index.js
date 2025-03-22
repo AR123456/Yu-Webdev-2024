2f8ee7add13a106c3a8e9f845b0cb04f58f587f9
AR123456 <coloradoz99@gmail.com>
2025-03-22 12:36:15 -0400

    final postman set u[

--- Section 24 Express and node/3.3 Postman/index.js
+++ Section 24 Express and node/3.3 Postman/index.js
@@ -6,6 +6,19 @@ app.get("/", (req, res) => {
   res.send("Hello from the server");
 });
 
+app.post("/register", (req, res) => {
+  // Do something with data from html form
+  res.sendStatus(201);
+});
+app.put("/user/joe", (req, res) => {
+  res.sendStatus(200);
+});
+app.patch("/user/joe", (req, res) => {
+  res.sendStatus(200);
+});
+app.delete("/user/joe", (req, res) => {
+  res.sendStatus(200);
+});
 app.listen(port, () => {
   console.log(`App is listening on port http://localhost:${port}`);
 });
