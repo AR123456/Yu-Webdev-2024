2ff1c9ffc8401b2c81f51be9bea44f3caa890fc9
AR123456 <coloradoz99@gmail.com>
2025-03-26 05:19:19 -0400

    ejs setting up server

---
+++ Section 25 EJS/4.0 EJS/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(port, () => {
+  console.log(`App is listening on port http://localhost:${port}`);
+});
