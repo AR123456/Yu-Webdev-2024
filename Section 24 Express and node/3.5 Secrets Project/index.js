6375d7fb25cf3808a8856a3a7ea553e706d82bdc
AR123456 <coloradoz99@gmail.com>
2025-03-26 05:17:44 -0400

    secrets project

---
+++ Section 24 Express and node/3.5 Secrets Project/index.js
@@ -0,0 +1,37 @@
+// user enters ILoveProgramming and clicks submit
+// deliver the secret page
+import express from "express";
+// dir
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+// body parser
+import bodyParser from "body-parser";
+// dynamic path to index html
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const app = express();
+const port = 3000;
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// function to check input does it = "ILoveProgramming"?
+function checkPassword(req, res, next) {
+  // console.log(req.body.password);
+  if (req.body.password !== "ILoveProgramming") {
+    console.log(req.body.password);
+  } else if (req.body.password == "ILoveProgramming") {
+    res.sendFile(__dirname + "/public/secret.html");
+  }
+
+  // do some checking
+  // if true put the path to secret page in var that gets passed in post
+  next();
+}
+app.use(checkPassword);
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+app.post("/check", (req, res) => {
+  // res.sendFile the secret page
+});
+app.listen(port, () => {
+  console.log(`App is listening on port http://localhost:${port}`);
+});
