755a7f13b1cc865513aca6f190d7a59b7a6ab826
AR123456 <coloradoz99@gmail.com>
2025-03-26 05:20:04 -0400

    working template string

--- Section 25 EJS/4.0 EJS/index.js
+++ Section 25 EJS/4.0 EJS/index.js
@@ -8,12 +8,12 @@ const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
-
+let weekend = false;
 function getDay(req, res, next) {
   const d = new Date();
 
   const day = d.getDay();
-  console.log(day);
+
   const dayNames = [
     "Sunday",
     "Monday",
@@ -23,13 +23,22 @@ function getDay(req, res, next) {
     "Friday",
     "Saturday",
   ];
+  if (day === 1 || day === 7) {
+    let weekend = true;
+  }
 
-  res.send(` <h1>Hey, its ${dayNames[day]}, !</h1>`);
+  // res.send(` <h1>Hey, its ${dayNames[day]}, !</h1>`);
   next();
 }
 app.use(getDay);
+
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/oldSchool/index.html");
+  if (weekend) {
+    res.sendFile(__dirname + "/oldSchool/indexWeekend.html");
+  } else {
+    res.sendFile(__dirname + "/oldSchool/indexWeekday.html");
+  }
+  // res.sendFile(__dirname + "/oldSchool/index.html");
 });
 
 app.listen(port, () => {
