16c2dbb4413fc62fa6730e5f3873cace1f4b7f68
AR123456 <coloradoz99@gmail.com>
2025-03-27 06:39:54 -0400

    working with template strings

--- Section 25 EJS/4.0 EJS/index.js
+++ Section 25 EJS/4.0 EJS/index.js
@@ -8,7 +8,7 @@ const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
-let weekend = false;
+
 function getDay(req, res, next) {
   const d = new Date();
 
@@ -23,18 +23,20 @@ function getDay(req, res, next) {
     "Friday",
     "Saturday",
   ];
-  // if (day === 1 || day === 7) {
-  //   let weekend = true;
-  // }
-  res.render(` <h1>Hey, its ${dayNames[day]}, !</h1>`);
-  // res.send(` <h1>Hey, its ${dayNames[day]}, !</h1>`);
+  if (day === 0 || day === 6) {
+    res.send(` <h1>Hey, its ${dayNames[day]}, Enjoy your weekend  !</h1>`);
+    return;
+  } else {
+    res.send(` <h1>Hey, its ${dayNames[day]}, Make it a great day  !</h1>`);
+    return;
+  }
+
   next();
 }
 app.use(getDay);
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/oldSchool/index.html");
-  //
 });
 
 app.listen(port, () => {
