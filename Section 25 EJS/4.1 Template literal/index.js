afead929dfdb8797603844afab944663ea2f9b24
AR123456 <coloradoz99@gmail.com>
2025-04-01 08:01:01 -0400

    same ex but pure js with template literal

---
+++ Section 25 EJS/4.1 Template literal/index.js
@@ -0,0 +1,42 @@
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  const data = {
+    title: "EJS Tags",
+    seconds: new Date().getSeconds(),
+    items: ["apple", "banana", "cherry"],
+    htmlContent: "<strong>This is some strong text</strong>",
+  };
+
+  const html = `   <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>${data.title}</title>
+    </head>
+    <body>
+      <h1>${data.title}</h1>
+      <p>Current second: ${data.seconds}</p>
+   
+      ${
+        data.seconds % 2 === 0
+          ? `<ul>${data.items
+              .map((fruit) => `<li>${fruit}</li>`)
+              .join("")}</ul>`
+          : "<p>No items to display</p>"
+      }
+      <p>${data.htmlContent}</p>
+      <footer>
+        <p>Copyright © from footer</p>
+      </footer>
+    </body>
+    </html>`;
+  res.send(html);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port http://localhost:${port}`);
+});
