25a1b2fe04305c89c0691e4cfccbf0291704d91a
AR123456 <coloradoz99@gmail.com>
2025-04-02 06:23:53 -0400

    ejs

--- Section 25 EJS/4.2+Passing+Data/index.js
+++ Section 25 EJS/4.2+Passing+Data/index.js
@@ -1,16 +1,36 @@
 import express from "express";
 import bodyParser from "body-parser";
 
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+function checkNameLength(req, res, next) {
+  const firstName = req.body.fname;
+  const lastName = req.body.lname;
+
+  console.log(firstName);
+  console.log(lastName);
+  next();
+}
+app.use(checkNameLength);
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/submit", (req, res) => {});
+app.post("/submit", (req, res) => {
+  console.log(req.body);
+  // checkNameLength(req.body);
+  // get data from form
+  // determine lettters in name
+  // write the number of letters to the  page
+});
+
 app.listen(port, () => {
   console.log(`Server running on port http://localhost:${port}`);
 });
