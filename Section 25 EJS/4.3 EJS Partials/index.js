490c9460aecfecd49b686631d117f5414ff58257
AR123456 <coloradoz99@gmail.com>
2025-04-03 06:55:49 -0400

    working on routes

--- Section 25 EJS/4.3 EJS Partials/index.js
+++ Section 25 EJS/4.3 EJS Partials/index.js
@@ -5,7 +5,12 @@ const port = 3000;
 app.get("/", (req, res) => {
   res.render("index.ejs");
 });
-
+app.get("/views/about", (req, res) => {
+  res.render("/views/about.ejs");
+});
+app.get("/views/contact", (req, res) => {
+  res.render("/views/contact.ejs");
+});
 app.listen(port, () => {
   console.log(`App is listening on port http://localhost:${port}`);
 });
