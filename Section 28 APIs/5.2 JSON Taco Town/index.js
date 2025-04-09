530c95ec6ede1f81a11f06ca992bd4ef16074ef7
AR123456 <coloradoz99@gmail.com>
2025-04-09 05:49:27 -0400

    getting closer to post

--- Section 28 APIs/5.2 JSON Taco Town/index.js
+++ Section 28 APIs/5.2 JSON Taco Town/index.js
@@ -19,7 +19,9 @@ app.get("/", (req, res) => {
 app.post("/recipe", (req, res) => {
   //Step 3: Write your code here to make this behave like the solution website.
   //  when user clicks of the button to submit send post request to recipe route
-  console.log(req.body);
+  console.log(req.body.choice);
+  // res.send(`<h2>${req.body.choice}</h2>`);
+  res.send(`<h2>${req.body.choice}</h2>`);
   //Step 4: Add code to views/index.ejs to use the recieved recipe object.
 });
 
