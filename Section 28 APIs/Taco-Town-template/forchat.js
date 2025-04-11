a67c2030fcb3b2ef980933c41a7847aabaebfde7
AR123456 <coloradoz99@gmail.com>
2025-04-11 07:23:50 -0400

    getting index.html showing

---
+++ Section 28 APIs/Taco-Town-template/forchat.js
@@ -0,0 +1,16 @@
+import express from "express";
+import bodyParser from "body-parser";
+import path from "path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
