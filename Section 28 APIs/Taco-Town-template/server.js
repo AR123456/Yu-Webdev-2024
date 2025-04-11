75262687a1dd866ed6ba69057dbe4f3d8afe9616
AR123456 <coloradoz99@gmail.com>
2025-04-11 07:24:16 -0400

    sending recipe object

--- Section 28 APIs/Taco-Town-template/server.js
+++ Section 28 APIs/Taco-Town-template/server.js
@@ -19,69 +19,26 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-// app.post("/recipe", (req, res) => {
-//   //Step 3: Write your code here to make this behave like the solution website.
-//   //  when user clicks of the button to submit send post request to recipe route
-//   // console.log(req.body.choice);
-//   // switch (req.body.choice) {
-//   //   case "chicken":
-//   //     recipe = JSON.parse(recipeJSON)[0];
-//   //     break;
-//   //   case "beef":
-//   //     recipe = JSON.parse(recipeJSON)[1];
-//   //     break;
-//   //   case "fish":
-//   //     recipe = JSON.parse(recipeJSON)[2];
-//   //     break;
-//   //   default:
-//   //     break;
-//   // }
-//   // console.log(recipe.ingredients.protein.name);
-//   //Step 4: Add code to views/index.ejs to use the recieved recipe object.
-//   // res.send(template);
-//   // res.redirect("/");
-// });
+app.post("/recipe", (req, res) => {
+  console.log(req.body.choice);
+  switch (req.body.choice) {
+    case "chicken":
+      recipe = JSON.parse(recipeJSON)[0];
+      break;
+    case "beef":
+      recipe = JSON.parse(recipeJSON)[1];
+      break;
+    case "fish":
+      recipe = JSON.parse(recipeJSON)[2];
+      break;
+    default:
+      break;
+  }
+  console.log(recipe.ingredients.protein.name);
+
+  res.send(recipe);
+});
 
 app.listen(port, () => {
   console.log(`App is listening on http://localhost:${port}`);
 });
-// const template = `<!DOCTYPE html>
-// <html>
-// <head>
-//   <title>Taco Recipes</title>
-//   <link rel="stylesheet" type="text/css" href="/styles/main.css">
-// </head>
-// <body>
-//   <h1>Taco Town 🌮</h1>
-
-//   <form action="/recipe" method="POST" class="buttons
-//   ">
-//     <button type="submit" value="chicken" name="choice">🍗</button>
-//     <button type="submit" value="beef" name="choice">🥩</button>
-//     <button type="submit" value="fish" name="choice">🐟</button>
-//   </form>
-//   <div id="recipeContainer" class="hidden">
-
-//       <h2 id="recipeTitle">
-//       ${recipe.name}
-//          </h2>
-//       <h3>Ingredients:</h3>
-//       <ul id="ingredientsList">
-//         <li>
-//         ${recipe.ingredients.protein.name},${
-//   recipe.ingredients.protein.preparation
-// }
-//         </li>
-//         <li>${recipe.ingredients.salsa.name} </li>
-//         ${recipe.ingredients.toppings.array.forEach((topping) => {
-//           console.log(topping);
-//           // <li>
-//           //   ${topping.quantity}of${topping.name}
-//           // </li>;
-//         })}
-
-//       </ul>
-
-//   </div>
-// </body>
-// </html>`;
