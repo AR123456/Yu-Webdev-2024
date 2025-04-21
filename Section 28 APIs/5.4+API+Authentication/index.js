db755f49eb8ef32b78f7562763499ea83025b6c6
AR123456 <coloradoz99@gmail.com>
2025-04-21 06:13:17 -0400

    basic auth

--- Section 28 APIs/5.4+API+Authentication/index.js
+++ Section 28 APIs/5.4+API+Authentication/index.js
@@ -16,9 +16,6 @@ app.get("/", (req, res) => {
 });
 
 app.get("/noAuth", async (req, res) => {
-  //TODO 2: Use axios to hit up the /random endpoint
-  //The data you get back should be sent to the ejs file as "content"
-  //Hint: make sure you use JSON.stringify to turn the JS object from axios into a string.
   try {
     const result = await axios.get(API_URL + "/random");
     res.render("index.ejs", { content: JSON.stringify(result.data) });
@@ -27,19 +24,18 @@ app.get("/noAuth", async (req, res) => {
   }
 });
 
-app.get("/basicAuth", (req, res) => {
-  //TODO 3: Write your code here to hit up the /all endpoint
-  //Specify that you only want the secrets from page 2
-  //HINT: This is how you can use axios to do basic auth:
-  // https://stackoverflow.com/a/74632908
-  /*
-   axios.get(URL, {
+app.get("/basicAuth", async (req, res) => {
+  try {
+    const result = await axios.get(API_URL + "/all?page=2", {
       auth: {
-        username: "abc",
-        password: "123",
+        username: yourUsername,
+        password: yourPassword,
       },
     });
-  */
+    res.render("index.ejs", { content: JSON.stringify(result.data) });
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
 });
 
 app.get("/apiKey", (req, res) => {
