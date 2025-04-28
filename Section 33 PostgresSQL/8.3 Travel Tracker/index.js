75a62ff8b249bf07da9f02de1cbaed487e014820
AR123456 <coloradoz99@gmail.com>
2025-04-28 07:34:38 -0400

    some notes and console logs

--- Section 33 PostgresSQL/8.3 Travel Tracker/index.js
+++ Section 33 PostgresSQL/8.3 Travel Tracker/index.js
@@ -1,8 +1,10 @@
 import express, { response } from "express";
 import bodyParser from "body-parser";
+// import the postgres module
 import pg from "pg";
 const app = express();
 const port = 3000;
+// boilerplate configuration
 const db = new pg.Client({
   user: "postgres",
   host: "localhost",
@@ -14,15 +16,19 @@ db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-
+// access to the home page
 app.get("/", async (req, res) => {
-  //Write your code here.
+  //query db for country visited - select column from table
   const result = await db.query("SELECT country_code FROM visited_countries");
+  // declare the var
   let countries = [];
+  // push just the country code to countries array
   result.rows.forEach((country) => {
     countries.push(country.country_code);
   });
+  // pass the array to ejs
   res.render("index.ejs", { countries: countries, total: countries.length });
+  // close the db
   db.end;
 });
 
