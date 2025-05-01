30e548649b675758cf8bf8d38c06f93a49e00ad3
AR123456 <coloradoz99@gmail.com>
2025-05-01 07:46:38 -0400

    adding input country to db

--- Section 33 PostgresSQL/8.3 Travel Tracker/index.js
+++ Section 33 PostgresSQL/8.3 Travel Tracker/index.js
@@ -40,10 +40,16 @@ app.post("/add", async (req, res) => {
     "SELECT country_code FROM countries WHERE country_name = $1",
     [input]
   );
-  // console.log(result);
-  const countryCode = result.rows[0].country_code;
-  console.log(countryCode);
-  // insert the country code into the db
+  // check for a null result
+  if (result.rows.length !== 0) {
+    //
+    const data = result.rows[0];
+    const countryCode = data.country_code;
+    await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", [
+      countryCode,
+    ]);
+    res.redirect("/");
+  }
 });
 
 app.listen(port, () => {
