33c1cfd60827ec0e046a718a9db785bb70b7a98b
AR123456 <coloradoz99@gmail.com>
2025-05-19 06:27:44 -0400

    create user in db

--- Section 35 Authentication/9 part one  Authentication/index.js
+++ Section 35 Authentication/9 part one  Authentication/index.js
@@ -1,8 +1,17 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "secrets",
+  password: "#",
+  port: 5432,
+});
+db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -22,8 +31,15 @@ app.get("/register", (req, res) => {
 app.post("/register", async (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
-  console.log(username);
-  console.log(password);
+  try {
+    await db.query(
+      "INSERT INTO users (email,password) VALUES($1,$2) RETURNING id",
+      [username, password]
+    );
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 app.post("/login", async (req, res) => {
