80086ae0fde5b1023c06615401a736a1a99e4c2b
AR123456 <coloradoz99@gmail.com>
2025-05-20 06:22:17 -0400

    working example

--- Section 35 Authentication/9 part one  Authentication -working/index.js
+++ Section 35 Authentication/9 part one  Authentication -working/index.js
@@ -4,12 +4,11 @@ import pg from "pg";
 
 const app = express();
 const port = 3000;
-
 const db = new pg.Client({
   user: "postgres",
   host: "localhost",
   database: "secrets",
-  password: " ",
+  password: "#",
   port: 5432,
 });
 db.connect();
@@ -24,54 +23,48 @@ app.get("/", (req, res) => {
 app.get("/login", (req, res) => {
   res.render("login.ejs");
 });
-
 app.get("/register", (req, res) => {
   res.render("register.ejs");
 });
-
 app.post("/register", async (req, res) => {
   const email = req.body.username;
   const password = req.body.password;
-
   try {
-    const checkResult = await db.query("SELECT * FROM users WHERE email = $1", [
+    const checkResult = await db.query("SELECT * FROM users WHERE email=$1", [
       email,
     ]);
-
     if (checkResult.rows.length > 0) {
-      res.send("Email already exists. Try logging in.");
+      res.send("Email already exists.  Try logging in instead.");
     } else {
       const result = await db.query(
-        "INSERT INTO users (email, password) VALUES ($1, $2)",
+        "INSERT INTO users (email,password) VALUES($1,$2)",
         [email, password]
       );
-      console.log(result);
       res.render("secrets.ejs");
     }
   } catch (err) {
     console.log(err);
   }
 });
-
 app.post("/login", async (req, res) => {
   const email = req.body.username;
   const password = req.body.password;
-
   try {
-    const result = await db.query("SELECT * FROM users WHERE email = $1", [
+    const result = await db.query("SELECT * FROM users WHERE email=$1", [
       email,
     ]);
+
     if (result.rows.length > 0) {
       const user = result.rows[0];
+      // user is =  { id: 2, email: 'test@test.com', password: '123456' }
       const storedPassword = user.password;
-
       if (password === storedPassword) {
         res.render("secrets.ejs");
       } else {
-        res.send("Incorrect Password");
+        res.send("Incorrect Password - but dont tell scammer that ");
       }
     } else {
-      res.send("User not found");
+      res.send("User not found - but dont tell scammer that ");
     }
   } catch (err) {
     console.log(err);
