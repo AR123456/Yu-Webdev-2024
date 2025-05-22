edcf81ca993450ad2f735238efa699f116d9f6ba
AR123456 <coloradoz99@gmail.com>
2025-05-22 05:37:25 -0400

    setting up passport

--- Section 35 Authentication/9.3-Sessions-and-Cookies/index.js
+++ Section 35 Authentication/9.3-Sessions-and-Cookies/index.js
@@ -4,6 +4,9 @@ import pg from "pg";
 import bcrypt from "bcrypt";
 // need session along with passport
 import session from "express-session";
+import passport from "passport";
+// local strategy
+import { Strategy } from "passport-local";
 
 const app = express();
 const port = 3000;
@@ -22,6 +25,9 @@ app.use(
     saveUninitialized: true,
   })
 );
+// passport must be after the session is initialized
+app.use(passport.initialize());
+app.use(passport.session());
 
 const db = new pg.Client({
   user: "postgres",
@@ -42,6 +48,15 @@ app.get("/login", (req, res) => {
 
 app.get("/register", (req, res) => {
   res.render("register.ejs");
+});
+// does the user have a active session ?
+app.get("./secrets", (req, res) => {
+  // comes from passport, gets saved into the request
+  if (req.isAuthenticated()) {
+    res.render("secrets.ejs");
+  } else {
+    res.redirect("/login");
+  }
 });
 
 app.post("/register", async (req, res) => {
@@ -76,35 +91,40 @@ app.post("/register", async (req, res) => {
 });
 
 app.post("/login", async (req, res) => {
-  const email = req.body.username;
-  const loginPassword = req.body.password;
-
-  try {
-    const result = await db.query("SELECT * FROM users WHERE email = $1", [
-      email,
-    ]);
-    if (result.rows.length > 0) {
-      const user = result.rows[0];
-      const storedHashedPassword = user.password;
-      bcrypt.compare(loginPassword, storedHashedPassword, (err, result) => {
-        if (err) {
-          console.error("Error comparing passwords:", err);
-        } else {
-          if (result) {
-            res.render("secrets.ejs");
-          } else {
-            res.send("Incorrect Password");
-          }
-        }
-      });
-    } else {
-      res.send("User not found");
-    }
-  } catch (err) {
-    console.log(err);
-  }
+  // passport will get user and email now no need to get from body parser
+  // const email = req.body.username;
+  // const loginPassword = req.body.password;
 });
-
+// username and password here match the "name=" attribute on the input in the front end log in form
+passport.use(
+  new Strategy(async function verify(username, password, cb) {
+    console.log(username);
+    try {
+      const result = await db.query("SELECT * FROM users WHERE email = $1", [
+        email,
+      ]);
+      if (result.rows.length > 0) {
+        const user = result.rows[0];
+        const storedHashedPassword = user.password;
+        bcrypt.compare(loginPassword, storedHashedPassword, (err, result) => {
+          if (err) {
+            console.error("Error comparing passwords:", err);
+          } else {
+            if (result) {
+              res.render("secrets.ejs");
+            } else {
+              res.send("Incorrect Password");
+            }
+          }
+        });
+      } else {
+        res.send("User not found");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  })
+);
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
